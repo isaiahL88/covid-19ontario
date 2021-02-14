5f51e59e1ce8b93ac79719a7a1d275075838bd1a
isaiahL88 <isaiah.ib@outlook.com>
2021-02-13 23:42:58 -0500

    almost fiinished commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+var PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log("listening at 3000"));
+app.listen(PORT, () => console.log("listening at 3000"));
 app.use(express.static('public'))
