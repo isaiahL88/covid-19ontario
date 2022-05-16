e8e754b554eec7fda67ef50dc844a7eb68e94bbc
isaiahL88 <isaiah.ib@outlook.com>
2022-05-16 15:30:32 -0400

    Fixied burger for mobile view

--- public/app.js
+++ public/app.js
@@ -7,7 +7,23 @@ function navSlider() {
     burger.addEventListener('click', () => {
         //Toggle Nav
         nav.classList.toggle('nav-active');
+        console.log('burger clicked');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+
     });
+
 
 }
 
